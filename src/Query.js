6a58168f6ed5de0ff29ff7da0c68e5464eccc100
Jack Kavanagh <jackkav@gmail.com>
2018-04-19 16:45:18 +0800

    can live update list with subscribeToMore

--- src/Query.js
+++ src/Query.js
@@ -1,33 +1,65 @@
 import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+const FEEDBACK_QUERY = gql`
+  {
+    feedbacks {
+      id
+      text
+    }
+  }
+`;
+const FEEDBACK_SUBSCRIPTION = gql`
+  subscription onFeedbackAdded {
+    feedbackAdded {
+      id
+      text
+    }
+  }
+`;
 export const Feedback = () => (
-  <Query
-    query={gql`
-      {
-        feedbacks{
-          id
-          text
-        }
-      }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :( {error.message}</p>;
-      return data.feedbacks.map(({ id, text }) => (
-        <div key={id}>
-          <p>{`${id}: ${text}`}</p>
-        </div>
-      ));
+  <Query query={FEEDBACK_QUERY}>
+    {({ subscribeToMore, ...result }) => {
+      return (
+        <FeedbackList
+          {...result}
+          subscribeToNewComments={() =>
+            subscribeToMore({
+              document: FEEDBACK_SUBSCRIPTION,
+              updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev;
+                const newFeedItem = subscriptionData.data.feedbackAdded;
+                return Object.assign({}, prev, {
+                  feedbacks: [newFeedItem, ...prev.feedbacks]
+                });
+              }
+            })
+          }
+        />
+      );
     }}
   </Query>
 );
+export class FeedbackList extends React.Component {
+  componentDidMount() {
+    this.props.subscribeToNewComments();
+  }
+  render() {
+    const { data, loading, error } = this.props;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :( {error.message}</p>;
+    return data.feedbacks.map(({ id, text }) => (
+      <div key={id}>
+        <p>{`${id}: ${text}`}</p>
+      </div>
+    ));
+  }
+}
 export const Books = () => (
   <Query
     query={gql`
       {
-        books{
+        books {
           title
           author
         }
